0c92109764352559c554e3e1441223c8be7c2471
Tsjippy <ewaldha@hotmail.com>
2026-06-01 20:42:13 +0200

    fixed: empty url bug

--- php/frontendposting.php
+++ php/frontendposting.php
@@ -11,17 +11,18 @@ function showFullscreen($postContent){
     // Check if content is just an hyperlink
     //find all urls in the page
     $regex 	= '(?xi)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))';
-    preg_match_all("#$regex#i", $postContent, $matches);
-    $url 	= $matches[0][0];
+    if(preg_match_all("#$regex#i", $postContent, $matches)){
+        $url 	= $matches[0][0];
 
-    //if the found url is the only post content
-    if($url == wp_strip_all_tags($postContent)){
-        //find the post id of the url
-        $postId	= url_to_postid($url);
+        //if the found url is the only post content
+        if($url == wp_strip_all_tags($postContent)){
+            //find the post id of the url
+            $postId	= url_to_postid($url);
 
-        // If a valid post id
-        if($postId > 0){
-            $postContent	= "[embed_page id='$postId']";
+            // If a valid post id
+            if($postId > 0){
+                $postContent	= "[embed_page id='$postId']";
+            }
         }
     }
 
